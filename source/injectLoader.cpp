487c902ceb48fa714cd4a6d03ebf0378f4e6a072
QuickLava <fastmovingvolcanomatter@gmail.com>
2024-07-05 19:35:06 -0700

    Reworked Into One-Time Multi-GCT Loader
    The further I got into implementing the approach the more it seemed like I was just re-implementing the Gecko codehandler itself. The plugin needed to be able to apply Fighter.pac injections, but to do that without needing to significantly re-configure the patches themselves I basically had to explicitly support every code type used in those injections.
    
    Ultimately that just didn't make a whole lot of sense, so I've pivoted into a slightly different direction: rather than explicitly support for every code type, I'll just borrow the codehandler itself! Using a hack inspired by Duke's Multi-GCT code, we load a given .gct into a static buffer, use a bootstrapper code to hijack the codehandler and point it at our newly loaded code, then use the same method to branch back to the code following the bootstrapper! We do this one .gct per frame, with pretty clean results in my testing! Very cool bit of a tech, if I do say so myself!

--- source/injectLoader.cpp
+++ source/injectLoader.cpp
@@ -6,6 +6,16 @@
 
 namespace lavaInjectLoader {
 
+    static unsigned long* const expansionReturnAddrPtr = 0x800017F0;
+    static unsigned long* const expansionCodeAddrPtr = expansionReturnAddrPtr + 0x1;
+    const unsigned long codeBufferLen = 0x2000;
+    const unsigned long returnCodeArr[0x6] =
+    {
+        0xC0000000, 0x00000002,                                                         // Gecko Pulse (2 Lines):
+        0x81FF0000 | ((unsigned long)expansionReturnAddrPtr & 0xFFFF), 0x38800000,      // lwz r15, ReturnAddrLo(r31)       li r4, 0x00
+        0x4E800020, 0x00000000                                                          // blr
+    };
+
     struct NJCTHeader
     {
         static const unsigned long magicVal = 0x4E4A4354; // NJCT
@@ -21,74 +31,93 @@ namespace lavaInjectLoader {
         unsigned long destinationAddress;
         unsigned long length;
     };
+    struct NJCTWordHeader
+    {
+        unsigned long destinationAddress;
+        unsigned long value;
+    };
 
     bool initialized = false;
 
-    void loadInjects()
+    void prepareGCT()
     {
-        initialized = true;
+        const static signed long status_unchecked = -1;
+        const static signed long status_notfound = 0;
+        const static signed long status_continuing = 1;
+        const static signed long status_finished = 2;
+        
+        static FAEntryInfo info;
+        static char pathBuf[0x80] = {};
+        static char codeBuf[codeBufferLen + sizeof(returnCodeArr)] = {};
+        static signed long execStatus = status_unchecked;
+        static unsigned long folderPathLen;
 
-        FAEntryInfo info;
-        FAHandle* streamHandlePtr;
-        NJCTHeader* currNJCTHeader;
-        NJCTPatchHeader* currPatchHeader;
-
-        unsigned long currNJCTBaseAddr;
-        unsigned long currNJCTPatchCount;
-
-        unsigned long currPatchWriteAddr;
-
-        char pathBuf[0x80];
-        static const unsigned long headerBufLen = (sizeof(NJCTHeader) > sizeof(NJCTPatchHeader)) ? sizeof(NJCTHeader) : sizeof(NJCTPatchHeader);
-        char headerBuf[headerBufLen];
-
-        OSReport("Loading Injects from folder!\n");
-        sprintf(pathBuf, "%spf/injects/", MOD_PATCH_DIR);
-        unsigned long folderPathLen = strlen(pathBuf);
-        strcpy(pathBuf + folderPathLen, "*.dat");
-        if (FAFsfirst(pathBuf, 0x20, &info) == 0)
+        if (execStatus == status_unchecked)
         {
-            do
+            OSReport("[lavaInjectLoader] Beginning Inject GCT from folder...\n");
+            sprintf(pathBuf, "%spf/injects/", MOD_PATCH_DIR);
+            folderPathLen = strlen(pathBuf);
+            strcpy(pathBuf + folderPathLen, "*.gct");
+            execStatus = (FAFsfirst(pathBuf, 0x20, &info) == 0) ? status_continuing : status_finished;
+        }
+        if (execStatus == status_continuing)
+        {
+            OSReport("[lavaInjectLoader] Loading Inject GCT from folder...\n");
+            strcpy(pathBuf + folderPathLen, info.name);
+            if (pathBuf[folderPathLen] == 0x0)
             {
-                strcpy(pathBuf + folderPathLen, info.name);
-                if (pathBuf[folderPathLen] == 0x0)
-                {
-                    strcpy(pathBuf + folderPathLen, info.shortname);
-                }
-                OSReport("%s\n", pathBuf);
-                streamHandlePtr = FAFopen(pathBuf, (const char*)0x8059C590);
-                if (streamHandlePtr != 0x00000000 && 
-                    FAFread(headerBuf, 1, sizeof(NJCTHeader), streamHandlePtr) == sizeof(NJCTHeader))
-                {
-                    currNJCTHeader = (NJCTHeader*)headerBuf;
-                    if (currNJCTHeader->magic == NJCTHeader::magicVal)
-                    {
-                        currNJCTPatchCount = currNJCTHeader->patchCount;
-                        currNJCTBaseAddr = currNJCTHeader->baseAddr;
+                strcpy(pathBuf + folderPathLen, info.shortname);
+            }
+            OSReport("%s\n", pathBuf);
+            FAHandle* streamHandlePtr = FAFopen(pathBuf, (const char*)0x8059C590);
+            if (streamHandlePtr != 0x00000000)
+            {
+                unsigned long fileLength = FAFread(codeBuf, 1, codeBufferLen, streamHandlePtr);
 
-                        while (currNJCTPatchCount > 0x0 &&
-                            FAFread(headerBuf, 1, sizeof(NJCTPatchHeader), streamHandlePtr) == sizeof(NJCTPatchHeader))
-                        {
-                            currPatchHeader = (NJCTPatchHeader*)headerBuf;
-                            currPatchWriteAddr = currPatchHeader->destinationAddress;
-                            if ((currPatchWriteAddr & 0x80000000) == 0)
-                            {
-                                currPatchWriteAddr += currNJCTBaseAddr;
-                            }
-                            FAFread((void*)currPatchWriteAddr, 1, currPatchHeader->length, streamHandlePtr);
-                            currNJCTPatchCount -= 0x1;
-                        }
+                if ((fileLength - 0x8 + sizeof(returnCodeArr)) < sizeof(codeBuf))
+                {
+                    unsigned long* returnStart = (unsigned long*)(codeBuf + fileLength - 0x8);
+
+
+                    for (unsigned long i = 0; i < sizeof(returnCodeArr); i++)
+                    {
+                        returnStart[i] = returnCodeArr[i];
                     }
                 }
-                FAFclose(streamHandlePtr);
-            } while (FAFsnext(&info) == 0);
+                else
+                {
+                    OSReport("[lavaInjectLoader] GCT overflowed buffer! Aborting!\n");
+                    execStatus = status_finished;
+                }
+            }
+            FAFclose(streamHandlePtr);
+            *expansionCodeAddrPtr = (unsigned long)codeBuf;
+
+            if (FAFsnext(&info) != 0)
+            {
+                execStatus = status_finished;
+            }
         }
+        else if (execStatus == status_finished)
+        {
+            OSReport("[lavaInjectLoader] Finished!\n");
+            *expansionCodeAddrPtr = 0x00ul;
+        }
+
         return;
+    }
+    void initLoaderAndInstallEntryPoint()
+    {
+        if (*expansionReturnAddrPtr != 0x00)
+        {
+            initialized = true;
+            SyringeCore::syInlineHook(0x800177B0, (void*)prepareGCT);
+        }
     }
 
     void Init()
     {
-        SyringeCore::syInlineHookRel(0x145190, reinterpret_cast<void*>(loadInjects), Modules::SORA_MELEE); //0x8084FBA0
+        SyringeCore::syInlineHookRel(0x145190, reinterpret_cast<void*>(initLoaderAndInstallEntryPoint), Modules::SORA_MELEE); //0x8084FBA4
     }
 
     void Destroy()
