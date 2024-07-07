dbd0ef6b40209e2b1375b45ca3d8c00def1cfc1c
QuickLava <fastmovingvolcanomatter@gmail.com>
2024-07-07 03:29:27 -0700

    Reworked to Allow On-Demand Load of Any Directory
    Essentially, we've moved away from the old approach where the loader was rigged up to fire once specifically after the Fighter.pac injects finished on a specific target directory. It's now a reusable loader which can now instead be invoked *whenever*, on *whatever* directory you want! To invoke, simply write the address of the target directory path string to the source path address, and that'll kickstart the rest of the loader, running it on that folder!
    
    Also moved to a slightly different main hookpoint, just to get off the one used by the Syriinge template project, since I imagine that could easily lead to conflicts lol.

--- source/injectLoader.cpp
+++ source/injectLoader.cpp
@@ -7,8 +7,10 @@
 namespace lavaInjectLoader {
 
     const char outputTag[] = "[lavaInjectLoader] ";
+    const char fighterInjectPath[] = "pf/injects/fighter/";
     static unsigned long* const expansionReturnAddrPtr = 0x800017F0;
     static unsigned long* const expansionCodeAddrPtr = expansionReturnAddrPtr + 0x1;
+    static unsigned long* const expansionSourcePathPtr = expansionCodeAddrPtr + 0x1;
     static const unsigned long codeBufferLen = 0x2000;
     static const unsigned long returnCodeArr[0x6] =
     {
@@ -19,85 +21,112 @@ namespace lavaInjectLoader {
 
     enum processStatus
     {
-        status_unchecked = -1,
-        status_notfound = 0,
-        status_continuing,
+        status_idle = 0,
+        status_working,
         status_finishing,
-        status_finished,
     };
-    static signed long execStatus = status_unchecked;
+    static signed long execStatus = status_idle;
 
     void prepareGCT()
     {
         static FAEntryInfo info;
         static char pathBuf[0x80] = {};
-        static char codeBuf[codeBufferLen + sizeof(returnCodeArr)] = {};
+        static char codeBuf[codeBufferLen + sizeof(returnCodeArr) - 0x8] = {};
         static unsigned long folderPathLen;
 
-        if (execStatus == status_finishing)
+        // Don't even try to process anything if we don't have a defined return address, just return immediately.
+        if (*expansionReturnAddrPtr == 0x00) return;
+
+        // If the previous round of work we did finished up our batch...
+        if (execStatus == status_finishing && (*expansionCodeAddrPtr == 0x00))
         {
-            execStatus = status_finished;
+            // ... set us back to idle...
+            execStatus = status_idle;
+            // ... zero out the source path to mark that we're open for a new batch...
+            *expansionSourcePathPtr = 0x00000000;
+            // ... and log that we've finished!
+            // Note: this is ahead of the below so we don't do this stuff on the same call where we queue the last .gct lol.
             OSReport("%sFinished!\n", outputTag);
         }
-        if (execStatus == status_finished) return;
-
-        if (execStatus == status_unchecked)
+        // If we're idle, and some work's been queued up...
+        if (execStatus == status_idle && (*expansionSourcePathPtr != 0x00))
         {
-            sprintf(pathBuf, "%spf/injects/", MOD_PATCH_DIR);
+            // ... then get the full path to the source directory...
+            sprintf(pathBuf, "%s%s", MOD_PATCH_DIR, *expansionSourcePathPtr);
+            // ... and report that we're attempting to apply it.
             OSReport("%sBeginning Injections from \"%s\"...\n", outputTag, pathBuf);
+            // Record the length of just the path so we know where to append filenames to.
             folderPathLen = strlen(pathBuf);
+            // Set up the pattern string for our file search...
             strcpy(pathBuf + folderPathLen, "*.gct");
-            execStatus = (FAFsfirst(pathBuf, 0x20, &info) == 0) ? status_continuing : status_finishing;
+            // ... and attempt to get the first file which matches it!
+            // If we find something, we'll switch to operating mode, if we don't we'll just skip to finishing.
+            execStatus = (FAFsfirst(pathBuf, 0x20, &info) == 0) ? status_working : status_finishing;
         }
-        if (execStatus == status_continuing && (*expansionCodeAddrPtr == 0x00))
+        // If we're working, and no .gct is currently waiting to be processed...
+        if (execStatus == status_working && (*expansionCodeAddrPtr == 0x00))
         {
+            // ... set up a new one!
+            // Set up the full path to the new target file (accounting for the case where we need short name instead).
             strcpy(pathBuf + folderPathLen, info.name);
             if (pathBuf[folderPathLen] == 0x0)
             {
                 strcpy(pathBuf + folderPathLen, info.shortname);
             }
+            // Report the path of the GCT we're loading...
             OSReport("%sLoading Inject GCT \"%s\"...\n", outputTag, pathBuf);
+            // ... then attempt to open a stream to it!
             FAHandle* streamHandlePtr = FAFopen(pathBuf, (const char*)0x8059C590);
+            // If that was successfull...
             if (streamHandlePtr != 0x00000000)
             {
+                // ... then read in as much of the file as we can fit in the buffer, recording the actual read in length.
                 unsigned long fileLength = FAFread(codeBuf, 1, codeBufferLen, streamHandlePtr);
-
-                if ((fileLength - 0x8 + sizeof(returnCodeArr)) < sizeof(codeBuf))
+                // If what we read in didn't reach or exceed the allowed amount...
+                if (fileLength < codeBufferLen)
                 {
+                    // ... then write the buffer address into place, so that the bootstrapper can apply it!
                     *expansionCodeAddrPtr = (unsigned long)codeBuf;
+                    // Then, write the return code into place!
                     unsigned long* returnStart = (unsigned long*)(codeBuf + fileLength - 0x8);
                     for (unsigned long i = 0; i < (sizeof(returnCodeArr) / sizeof(returnCodeArr[0])); i++)
                     {
                         returnStart[i] = returnCodeArr[i];
                     }
                 }
+                // If it did exceed the allowed space though...
                 else
                 {
+                    // ... report it, and abort loading the batch.
                     OSReport("%sGCT overflowed buffer! Aborting!\n", outputTag);
                     execStatus = status_finishing;
                 }
-            }
-            FAFclose(streamHandlePtr);
 
+                // Regardless, ensure we close the stream handle before we exit!
+                FAFclose(streamHandlePtr);
+            }
+            // Lastly, attempt to queue up the next .gct to load! If we fail though...
             if (FAFsnext(&info) != 0)
             {
+                // ... then we're finished, so mark that we're finishing up the batch!
                 execStatus = status_finishing;
             }
         }
-
+        
         return;
     }
-    void installEntryPoint()
+    void doFighterInjectLoads()
     {
-        if (execStatus == status_unchecked && *expansionReturnAddrPtr != 0x00)
+        if (*expansionReturnAddrPtr != 0x00)
         {
-            SyringeCore::syInlineHook(0x800177B0, (void*)prepareGCT);
+            *expansionSourcePathPtr = (unsigned long)fighterInjectPath;
         }
     }
 
     void Init()
     {
-        SyringeCore::syInlineHookRel(0x145190, reinterpret_cast<void*>(installEntryPoint), Modules::SORA_MELEE); //0x8084FBA4
+        SyringeCore::syInlineHook(0x800177A8, reinterpret_cast<void*>(prepareGCT));
+        SyringeCore::syInlineHookRel(0x145190, reinterpret_cast<void*>(doFighterInjectLoads), Modules::SORA_MELEE); //0x8084FBA4
     }
 
     void Destroy()
