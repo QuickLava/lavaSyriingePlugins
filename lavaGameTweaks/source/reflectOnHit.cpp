fa5bb9be435fb17cf3f28bbccad1d857568bcf67
QuickLava <fastmovingvolcanomatter@gmail.com>
2024-10-04 07:17:52 -0700

    Setup Hook to Disable Clanking Against Projectiles

--- lavaGameTweaks/source/reflectOnHit.cpp
+++ lavaGameTweaks/source/reflectOnHit.cpp
@@ -55,10 +55,53 @@ namespace reflectOnHit
 			}
 		}
 	}
+	// Returns 1 if clank is allowed, 0 otherwise!
+	bool validateClankBody(StageObject* attackerPtr, StageObject* targetPtr)
+	{
+		const char fmtStr[] = "%s- %s (Cat: 0x%04X)\n";
+
+		bool result = 1;
+
+		int attackerCategory = fighterHooks::getCatPtr((gfTask*)attackerPtr);
+		int targetCategory = fighterHooks::getCatPtr((gfTask*)targetPtr);
+		OSReport_N("%sChecking Collision Between:\n", outputTag);
+		OSReport_N(fmtStr, outputTag, attackerPtr->m_taskName, attackerCategory);
+		OSReport_N(fmtStr, outputTag, targetPtr->m_taskName, targetCategory);
+		if ((attackerCategory == 0xA && (targetCategory == 0xB || targetCategory == 0xC)) ||
+			((attackerCategory == 0xB || attackerCategory == 0xC) && targetCategory == 0xA))
+		{
+			OSReport_N("%s- Clank Disabled!\n", outputTag);
+			result = 0;
+		}
+		return result;
+	}
+	asm void disableArticleAndItemClankHook()
+	{
+		nofralloc
+			mflr r31;			  // Backup LR in a non-volatile register!
+			                      // Call main function body!
+			lwz r3, 0x8(r4);	  // param1 is Attacker StageObject*
+			mr r4, r5;			  // param2 is Target StageObject*
+			bl validateClankBody; // Call!
+			
+			mtlr r31;			  // Restore Trampoline LR!
+			cmplwi r3, 0x01;	  // Compare the return value to 1...
+			beq skipLRHack;		  // ... and if it's 1 we'll continue our function like normal!
+								  // Otherwise, we're gonna return from the function early!
+			lmw r4, 0x0C(r1);	  // Restore all but r3, so we can pass back our return value!
+			lwz r1, 0x00(r1);     // Deallocate Trampoline stack frame!
+			lwz r1, 0x00(r1);     // Deallocate Pre-Trampoline stack frame!
+			lwz r0, 0x04(r1);     // Grab Pre-Trampoline LR...
+			mtlr r0;              // ... and write it to LR so we return there instead!
+		skipLRHack:
+			blr; 				  // Return, either to Trampoline or to Pre-Trampoline!
+	}
 
 	void registerHooks()
 	{
 		fighterHooks::ftCallbackMgr::registerOnUpdateCallback(reflectBoxCB);
 		fighterHooks::ftCallbackMgr::registerOnAttackCallback(reflectTargetOnHitCallback);
+		// Disable Fighter vs Item/Article Clanks @ 0x8074651C: 0x0C bytes into symbol "checkRebound/[soCollisionAttackModuleImpl]/so_collision_a"
+		SyringeCore::syInlineHookRel(0x3BB08, reinterpret_cast<void*>(disableArticleAndItemClankHook), Modules::SORA_MELEE);
 	}
 }
