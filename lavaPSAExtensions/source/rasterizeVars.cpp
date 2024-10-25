efc3d3e12ed3643a06c8f0dbe756e9d75485470a
QuickLava <fastmovingvolcanomatter@gmail.com>
2024-10-25 01:51:05 -0700

    Fixed Outdated Enum Member Name Prefix
    Forgot to change this from when i moved from "argFlags" to "argTypes".

--- lavaPSAExtensions/source/rasterizeVars.cpp
+++ lavaPSAExtensions/source/rasterizeVars.cpp
@@ -47,30 +47,30 @@ namespace rasterizeVars
     };
     enum typeLibraryIndices
     {
-        fli_NULL = -1,
-        fli_033,
-        fli_0011111113,
-        fli_0011111111111113,
-        fli_00011101113001111111001,
-        fli_01,
-        fli_000,
-        fli_030,
-        fli_00111,
-        fli_00555,
-        fli_00111100,
-        fli_000001111111000,
-        fli_0001111111111310,
-        fli_00000000111000330,
-        fli_00011110003003030,
-        fli_0111,
-        fli_1,
-        fli_111,
-        fli_0000,
-        fli_00000,
-        fli_000011,
-        fli_COUNT
+        tli_NULL = -1,
+        tli_033,
+        tli_0011111113,
+        tli_0011111111111113,
+        tli_00011101113001111111001,
+        tli_01,
+        tli_000,
+        tli_030,
+        tli_00111,
+        tli_00555,
+        tli_00111100,
+        tli_000001111111000,
+        tli_0001111111111310,
+        tli_00000000111000330,
+        tli_00011110003003030,
+        tli_0111,
+        tli_1,
+        tli_111,
+        tli_0000,
+        tli_00000,
+        tli_000011,
+        tli_COUNT
     };
-    const argTypeBank typeBankLibrary[fli_COUNT] =
+    const argTypeBank typeBankLibrary[tli_COUNT] =
     {
         // 033
         { 
@@ -174,59 +174,59 @@ namespace rasterizeVars
     const cmdWhitelistEntry allowedCommands[] =
     {
         // Graphics Commands
-        { 0x11, 0x15, 0x00, fli_033},                      // [11150300] Terminate Graphic Effect
-        { 0x11, 0x01, 0x00, fli_0011111113 },              // [11010A00] Graphic Effect (Attached)
-        { 0x11, 0x02, 0x00, fli_0011111113 },              // [11020A00] Graphic Effect (Attached 2)
-        { 0x11, 0x19, 0x00, fli_0011111113 },              // [11190A00] Graphic Effect (Attached 19)
-        { 0x11, 0x00, 0x00, fli_0011111111111113 },        // [11001000] Graphic Effect
-        { 0x11, 0x1A, 0x00, fli_0011111111111113 },        // [111A1000] Graphic Effect (Stepping)
-        { 0x11, 0x1B, 0x00, fli_0011111111111113 },        // [111B1000] Graphic Effect (Landing)
-        { 0x11, 0x1C, 0x00, fli_0011111111111113 },        // [111C1000] Graphic Effect (Tumbling)
-        { 0x11, 0x03, 0x00, fli_00011101113001111111001 }, // [11031400] Sword Glow
-        { 0x11, 0x04, 0x00, fli_00011101113001111111001 }, // [11041700] Sword/Hammer Glow
+        { 0x11, 0x15, 0x00, tli_033},                      // [11150300] Terminate Graphic Effect
+        { 0x11, 0x01, 0x00, tli_0011111113 },              // [11010A00] Graphic Effect (Attached)
+        { 0x11, 0x02, 0x00, tli_0011111113 },              // [11020A00] Graphic Effect (Attached 2)
+        { 0x11, 0x19, 0x00, tli_0011111113 },              // [11190A00] Graphic Effect (Attached 19)
+        { 0x11, 0x00, 0x00, tli_0011111111111113 },        // [11001000] Graphic Effect
+        { 0x11, 0x1A, 0x00, tli_0011111111111113 },        // [111A1000] Graphic Effect (Stepping)
+        { 0x11, 0x1B, 0x00, tli_0011111111111113 },        // [111B1000] Graphic Effect (Landing)
+        { 0x11, 0x1C, 0x00, tli_0011111111111113 },        // [111C1000] Graphic Effect (Tumbling)
+        { 0x11, 0x03, 0x00, tli_00011101113001111111001 }, // [11031400] Sword Glow
+        { 0x11, 0x04, 0x00, tli_00011101113001111111001 }, // [11041700] Sword/Hammer Glow
 
         // Hitbox Commands
-        { 0x06, 0x01, 0x00, fli_01 },                      // [06010200] Change Hitbox Damage
-        { 0x06, 0x02, 0x00, fli_01 },                      // [06020200] Change Hitbox Size
-        { 0x06, 0x17, 0x00, fli_000 },                     // [06170300] Defensive Collision
-        { 0x06, 0x1E, 0x00, fli_030 },                     // [061E0300] Defensive Collision: Flip Toggle
-        { 0x06, 0x1B, 0x00, fli_00111 },                   // [061B0500] Move Hitbox
-        { 0x06, 0x0F, 0x00, fli_00555 },                   // [060F0500] Throw Collision
-        { 0x06, 0x0A, 0x00, fli_00111100 },                // [060A0800] Catch Collision
-        { 0x06, 0x00, 0x00, fli_000001111111000 },         // [06000D00] Offensive Collision
-        { 0x06, 0x2B, 0x00, fli_000001111111000 },         // [062B0D00] Thrown Collision
-        { 0x06, 0x15, 0x00, fli_000001111111000 },         // [06150F00] Special Offensive Collision
-        { 0x06, 0x2C, 0x00, fli_000001111111000 },         // [062C0F00] Special Collateral Collision
-        { 0x06, 0x24, 0x00, fli_0001111111111310 },        // [06241000] Generate Defensive Collision Bubble
-        { 0x06, 0x0E, 0x00, fli_00000000111000330 },       // [060E1100] Throw Attack Collision
-        { 0x06, 0x10, 0x00, fli_00011110003003030 },       // [06101100] Inert Collision
+        { 0x06, 0x01, 0x00, tli_01 },                      // [06010200] Change Hitbox Damage
+        { 0x06, 0x02, 0x00, tli_01 },                      // [06020200] Change Hitbox Size
+        { 0x06, 0x17, 0x00, tli_000 },                     // [06170300] Defensive Collision
+        { 0x06, 0x1E, 0x00, tli_030 },                     // [061E0300] Defensive Collision: Flip Toggle
+        { 0x06, 0x1B, 0x00, tli_00111 },                   // [061B0500] Move Hitbox
+        { 0x06, 0x0F, 0x00, tli_00555 },                   // [060F0500] Throw Collision
+        { 0x06, 0x0A, 0x00, tli_00111100 },                // [060A0800] Catch Collision
+        { 0x06, 0x00, 0x00, tli_000001111111000 },         // [06000D00] Offensive Collision
+        { 0x06, 0x2B, 0x00, tli_000001111111000 },         // [062B0D00] Thrown Collision
+        { 0x06, 0x15, 0x00, tli_000001111111000 },         // [06150F00] Special Offensive Collision
+        { 0x06, 0x2C, 0x00, tli_000001111111000 },         // [062C0F00] Special Collateral Collision
+        { 0x06, 0x24, 0x00, tli_0001111111111310 },        // [06241000] Generate Defensive Collision Bubble
+        { 0x06, 0x0E, 0x00, tli_00000000111000330 },       // [060E1100] Throw Attack Collision
+        { 0x06, 0x10, 0x00, tli_00011110003003030 },       // [06101100] Inert Collision
 
         // Bone Commands
-        { 0x03, 0x02, 0x00, fli_01 },                      // [03020200] Set Bone Rotation (X)
-        { 0x03, 0x03, 0x00, fli_01 },                      // [03030200] Set Bone Rotation (Y)
-        { 0x03, 0x04, 0x00, fli_01 },                      // [03040200] Set Bone Rotation (Z)
-        { 0x03, 0x07, 0x00, fli_01 },                      // [03070200] Set Bone Scale (X)
-        { 0x03, 0x08, 0x00, fli_01 },                      // [03080200] Set Bone Scale (Y)
-        { 0x03, 0x09, 0x00, fli_01 },                      // [03090200] Set Bone Scale (Z)
-        { 0x03, 0x0C, 0x00, fli_01 },                      // [030C0200] Set Bone Translation (X)
-        { 0x03, 0x0D, 0x00, fli_01 },                      // [030D0200] Set Bone Translation (Y)
-        { 0x03, 0x0E, 0x00, fli_01 },                      // [030E0200] Set Bone Translation (Z)
-        { 0x03, 0x01, 0x00, fli_0111 },                    // [03010400] Set Bone Rotation (XYZ)
-        { 0x03, 0x06, 0x00, fli_0111 },                    // [03060400] Set Bone Scale (XYZ)
-        { 0x03, 0x0B, 0x00, fli_0111 },                    // [030B0400] Set Bone Translation (XYZ)
+        { 0x03, 0x02, 0x00, tli_01 },                      // [03020200] Set Bone Rotation (X)
+        { 0x03, 0x03, 0x00, tli_01 },                      // [03030200] Set Bone Rotation (Y)
+        { 0x03, 0x04, 0x00, tli_01 },                      // [03040200] Set Bone Rotation (Z)
+        { 0x03, 0x07, 0x00, tli_01 },                      // [03070200] Set Bone Scale (X)
+        { 0x03, 0x08, 0x00, tli_01 },                      // [03080200] Set Bone Scale (Y)
+        { 0x03, 0x09, 0x00, tli_01 },                      // [03090200] Set Bone Scale (Z)
+        { 0x03, 0x0C, 0x00, tli_01 },                      // [030C0200] Set Bone Translation (X)
+        { 0x03, 0x0D, 0x00, tli_01 },                      // [030D0200] Set Bone Translation (Y)
+        { 0x03, 0x0E, 0x00, tli_01 },                      // [030E0200] Set Bone Translation (Z)
+        { 0x03, 0x01, 0x00, tli_0111 },                    // [03010400] Set Bone Rotation (XYZ)
+        { 0x03, 0x06, 0x00, tli_0111 },                    // [03060400] Set Bone Scale (XYZ)
+        { 0x03, 0x0B, 0x00, tli_0111 },                    // [030B0400] Set Bone Translation (XYZ)
 
         // Char Pos/Rot Commands
-        { 0x05, 0x05, 0x00, fli_1 },                       // [05050100] Change Model Size
-        { 0x05, 0x06, 0x00, fli_111 },                     // [05060300] Rotate Character Model
-        { 0x05, 0x09, 0x00, fli_111 },                     // [05090300] Set Character Position
-        { 0x05, 0x0A, 0x00, fli_111 },                     // [050A0300] Set Character Position 2
-        { 0x05, 0x0B, 0x00, fli_111 },                     // [050B0300] Set Character Position (Relative)
+        { 0x05, 0x05, 0x00, tli_1 },                       // [05050100] Change Model Size
+        { 0x05, 0x06, 0x00, tli_111 },                     // [05060300] Rotate Character Model
+        { 0x05, 0x09, 0x00, tli_111 },                     // [05090300] Set Character Position
+        { 0x05, 0x0A, 0x00, tli_111 },                     // [050A0300] Set Character Position 2
+        { 0x05, 0x0B, 0x00, tli_111 },                     // [050B0300] Set Character Position (Relative)
 
         // Flash Overlay Commands
-        { 0x21, 0x01, 0x00, fli_0000 },                    // [21010400] Flash Overlay Effect
-        { 0x21, 0x02, 0x00, fli_00000 },                   // [21020500] Change Flash Overlay Color
-        { 0x21, 0x07, 0x00, fli_00000 },                   // [21070500] Change Flash Light Color
-        { 0x21, 0x05, 0x00, fli_000011 },                  // [21050600] Flash Light Effect
+        { 0x21, 0x01, 0x00, tli_0000 },                    // [21010400] Flash Overlay Effect
+        { 0x21, 0x02, 0x00, tli_00000 },                   // [21020500] Change Flash Overlay Color
+        { 0x21, 0x07, 0x00, tli_00000 },                   // [21070500] Change Flash Light Color
+        { 0x21, 0x05, 0x00, tli_000011 },                  // [21050600] Flash Light Effect
     };
     const u32 allowedCommandCount = sizeof(allowedCommands) / sizeof(cmdWhitelistEntry);
 
