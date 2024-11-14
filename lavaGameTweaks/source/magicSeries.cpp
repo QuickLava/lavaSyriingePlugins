545a1477fb9b21e19c8bfafd53381f9eb675e0fa
QuickLava <fastmovingvolcanomatter@gmail.com>
2024-11-14 12:11:03 -0800

    Redid MagicSeries Cancel Logic w/ TransitionModule
    Makes the actual cancels properly accurate to Lucario! No longer allows cancelling a move into itself; nothing cancels into jabs and tilts can't cancel into other tilts!
    
    Also deleted old code from early reflect exploration stuff.

--- lavaGameTweaks/source/magicSeries.cpp
+++ lavaGameTweaks/source/magicSeries.cpp
@@ -1,20 +1,17 @@
 #include "magicSeries.h"
+#include <ft/fighter.h>
 
 namespace magicSeries
 {
     const char outputTag[] = "[lavaMagicSeries] ";
-    enum statusIDs
+    enum actionStatusIDs
     {
-        SID_ATTACK_1 = 0x24,
-        SID_ATTACK_100 = 0x25,
-        SID_ATTACK_DASH = 0x26,
-        SID_ATTACK_S3 = 0x27,
-        SID_ATTACK_HI3 = 0x28,
-        SID_ATTACK_LW3 = 0x29,
-        SID_ATTACK_S4S = 0x2C,
-        SID_ATTACK_LW4 = 0x2F,
-        SID_ATTACK_HI4 = 0x32,
-        SID_ATTACK_AIR = 0x33,
+        ASID_ATTACK_S3 = 0x2764,
+        ASID_ATTACK_HI3 = 0x2765,
+        ASID_ATTACK_LW3 = 0x2766,
+        ASID_ATTACK_S4_START = 0x2768,
+        ASID_ATTACK_HI4_START = 0x276B,
+        ASID_ATTACK_LW4_START = 0x276E,
     };
 
     void magicSeriesCallback(Fighter* attacker, StageObject* target, float damage)
@@ -23,73 +20,46 @@ namespace magicSeries
         soModuleEnumeration* moduleEnum = &attacker->m_moduleAccesser->m_moduleEnumeration;
         if (moduleEnum != NULL)
         {
-            int currSituation = moduleEnum->m_situationModule->getKind();
             int currStatus = moduleEnum->m_statusModule->getStatusKind();
+            soTransitionModule* transitionModule = ((soStatusModuleImpl*)moduleEnum->m_statusModule)->m_transitionModule;
+            switch (currStatus)
+            {
+            case Fighter::Status_Attack: case Fighter::Status_Attack_100: case Fighter::Status_Attack_Dash: 
+            case Fighter::Status_Attack_S3: case Fighter::Status_Attack_Hi3: case Fighter::Status_Attack_Lw3:
+            {
+                transitionModule->enableTermGroup(0x4);
+                transitionModule->unableTermAll(0x4);
+                if (currStatus <= Fighter::Status_Attack_Dash)
+                {
+                    transitionModule->enableTerm(ASID_ATTACK_S3, 0x4);
+                    transitionModule->enableTerm(ASID_ATTACK_HI3, 0x4);
+                    transitionModule->enableTerm(ASID_ATTACK_LW3, 0x4);
+                }
+                transitionModule->enableTerm(ASID_ATTACK_S4_START, 0x4);
+                transitionModule->enableTerm(ASID_ATTACK_HI4_START, 0x4);
+                transitionModule->enableTerm(ASID_ATTACK_LW4_START, 0x4);
 
-            if (currSituation == 0x00)
-            {
-                switch (currStatus)
-                {
-                case SID_ATTACK_1: case SID_ATTACK_100: case SID_ATTACK_DASH:
-                {
-                    moduleEnum->m_statusModule->enableTransitionTermGroup(0x7);
-                    OSReport_N("- Enabled Jump Cancels!\n");
-                }
-                case SID_ATTACK_S3: case SID_ATTACK_HI3: case SID_ATTACK_LW3:
-                {
-                    moduleEnum->m_statusModule->enableTransitionTermGroup(0x4);
-                    OSReport_N("- Enabled Attack Cancels!\n");
-                }
-                case SID_ATTACK_S4S: case SID_ATTACK_LW4: case SID_ATTACK_HI4:
-                {
-                    moduleEnum->m_statusModule->enableTransitionTermGroup(0x1);
-                    OSReport_N("- Enabled Special Cancels!\n");
-                    break;
-                }
-                }
+                OSReport_N("- Enabled Attack Cancels!\n");
             }
-            else if (currSituation == 0x02 && currStatus == SID_ATTACK_AIR)
+            case Fighter::Status_Attack_S4: case Fighter::Status_Attack_Lw4: case Fighter::Status_Attack_Hi4: case Fighter::Status_Attack_Air:
             {
-                moduleEnum->m_statusModule->enableTransitionTermGroup(0xB);
-                OSReport_N("- Enabled Air Special Cancels!\n");
+                if (moduleEnum->m_situationModule->getKind() == 0x00)
+                {
+                    transitionModule->enableTermGroup(0x1);
+                }
+                else
+                {
+                    transitionModule->enableTermGroup(0xB);
+                }
+                OSReport_N("- Enabled Special Cancels!\n");
+                break;
+            }
             }
         }
     }
-    /*
-    void itemAttackCallback(Fighter* attacker, StageObject* target, float damage, BaseItem* item)
-    {
-        typedef void (*reflectFunc)(BaseItem*);
-        reflectFunc onReflect = (reflectFunc)0x80995EE0;
 
-        soEffectModule* effectModule = item->m_moduleAccesser->getEffectModule();
-        Vec3f transRot = { 0.0f, 0.0f, 0.0f };
-        effectModule->req(2.0f, 54, &transRot, &transRot, 0, -1);
-
-        soSoundModule* soundModule = item->m_moduleAccesser->getSoundModule();
-        soundModule->playSE(snd_se_narration_one, 1, 1, 0);
-
-        onReflect(item);
-    }
-    void articleAttackCallback(Fighter* attacker, StageObject* target, float damage, Weapon* article)
-    {
-        typedef void (*reflectFunc)(Weapon*);
-
-        soEffectModule* effectModule = article->m_moduleAccesser->getEffectModule();
-        Vec3f transRot = { 0.0f, 0.0f, 0.0f };
-        effectModule->req(2.0f, 53, &transRot, &transRot, 0, -1);
-
-        soSoundModule* soundModule = article->m_moduleAccesser->getSoundModule();
-        soundModule->playSE(snd_se_narration_two, 1, 1, 0);
-
-        int* weaponVTable9 = ((int**)article)[0xF] + 0x49;
-        reflectFunc funcPtr = (reflectFunc)weaponVTable9[0x42];
-        funcPtr(article);
-    }
-    */
     void registerHooks()
     {
         fighterHooks::ftCallbackMgr::registerOnAttackCallback(magicSeriesCallback);
-        //fighterHooks::ftCallbackMgr::registerOnAttackItemCallback(itemAttackCallback);
-        //fighterHooks::ftCallbackMgr::registerOnAttackArticleCallback(articleAttackCallback);
     }
 }
