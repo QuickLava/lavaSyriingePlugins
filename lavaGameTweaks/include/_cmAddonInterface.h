d81e3c99950992212aaa30ba69ceee90211c718a
QuickLava <fastmovingvolcanomatter@gmail.com>
2024-12-07 01:57:28 -0800

    Initial CM_Addon Interface System Impl.
    Establishes a new system for loading in the auto-generated "Index Files" that the Code Menu Builder can spit out for Addons. These contain the INDEX values of every included line within the Addon, which when coupled with careful control over the order in which Lines are declared *within* an Addon can allow proper communication with Syriinge.

---
+++ lavaGameTweaks/include/_cmAddonInterface.h
@@ -0,0 +1,68 @@
+#ifndef LAVA_CM_ADDON_INTERFACE_H_V1
+#define LAVA_CM_ADDON_INTERFACE_H_V1
+
+#include <cstdlib>
+#include "logUtils.h"
+
+namespace codeMenu
+{
+    enum lineType
+    {
+        lt_SELECTION = 0,
+        lt_INTEGER,
+        lt_FLOATING,
+        lt_SUB_MENU,
+        lt_COMMENT,
+        lt_PRINT
+    };
+    struct cmSelectionLine
+    {
+        u16 m_size;
+        u16 m_type;
+        u8 m_flags;
+        u8 m_color;
+        u8 m_textOffset;
+        u8 m_lineNum;
+        u32 m_value;
+        u16 m_upOff;
+        u16 m_downOff;
+        u32 m_default;
+        u32 m_max;
+    };
+    struct cmIntegerLine
+    {
+        u16 m_size;
+        u16 m_type;
+        u8 m_flags;
+        u8 m_color;
+        u8 m_textOffset;
+        u8 m_lineNum;
+        u32 m_value;
+        u16 m_upOff;
+        u16 m_downOff;
+        u32 m_default;
+        u32 m_max;
+        u32 m_min;
+        u32 m_speed;
+    };
+    struct cmFloatingLine
+    {
+        u16 m_size;
+        u16 m_type;
+        u8 m_flags;
+        u8 m_color;
+        u8 m_textOffset;
+        u8 m_lineNum;
+        float m_value;
+        u16 m_upOff;
+        u16 m_downOff;
+        float m_default;
+        float m_max;
+        float m_min;
+        float m_speed;
+    };
+
+    bool loadCodeMenuAddonLOCsToBuffer(const char* addonShortName, u32* buffer, u32 entryCount);
+}
+
+#endif
---
+++ lavaGameTweaks/source/_cmAddonInterface.cpp
@@ -0,0 +1,31 @@
+#include <string.h>
+#include <sy_core.h>
+#include <revolution/FA.h>
+#include "_cmAddonInterface.h"
+
+namespace codeMenu
+{
+    const char addonFolderPath[] = "Source/CM_Addons/";
+    const char locFileFilename[] = "/idx.dat";
+    const u32 addonShortNameMaxLen = 0x8;
+    const u32 pathBufferLen = sizeof(MOD_PATCH_DIR) + sizeof(addonFolderPath) + addonShortNameMaxLen + sizeof(locFileFilename);
+
+    bool loadCodeMenuAddonLOCsToBuffer(const char* addonShortName, u32* buffer, u32 entryCount)
+    {
+        bool result = 0;
+
+        char pathBuffer[pathBufferLen];
+        if (strlen(addonShortName) <= addonShortNameMaxLen)
+        {
+            sprintf(pathBuffer, "%s%s%s%s", MOD_PATCH_DIR, addonFolderPath, addonShortName, locFileFilename);
+            FAHandle* streamHandlePtr = FAFopen(pathBuffer, (const char*)0x8059C590);
+            if (streamHandlePtr != NULL)
+            {
+                u32 expectedLength = entryCount * 4;
+                result = FAFread(buffer, 1, expectedLength, streamHandlePtr) == expectedLength;
+            }
+        }
+
+        return result;
+    }
+}
