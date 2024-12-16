6bd71669af5aeea838cca1f2718b2e766dcc3f6a
QuickLava <fastmovingvolcanomatter@gmail.com>
2024-12-15 18:56:47 -0800

    Turned Logging Back on By Default

--- lib/logUtils.h
+++ lib/logUtils.h
@@ -4,7 +4,7 @@
 #include <os/OSError.h>
 #include <types.h>
 
-#define __LOG_UTILS_ALLOW_LOGGING false
+#define __LOG_UTILS_ALLOW_LOGGING true
 #if __LOG_UTILS_ALLOW_LOGGING
 // Logging Enabled (via logUtils.h)!
 #define OSReport_N OSReport
