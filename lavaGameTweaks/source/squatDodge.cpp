ef34fcc940c5781dd9271f0b23544ef222ed4a59
QuickLava <fastmovingvolcanomatter@gmail.com>
2025-03-31 22:19:35 -0700

    Implemented Walljump Out of Special (Fall)
    Also took the opportunity to rearrange everything into a switch statement just to clean things up a bit.

--- lavaGameTweaks/source/squatDodge.cpp
+++ lavaGameTweaks/source/squatDodge.cpp
@@ -37,122 +37,156 @@ namespace squatDodge
             u32 walljumpSpentTemp = walljumpSpent;
             u32 parryBufferedTemp = parryBuffered;
 
-            if (currStatus == Fighter::Status_Jump_Squat)
+            switch (currStatus)
             {
-                if (moduleAccesser->m_enumerationStart->m_controllerModule->getTrigger().m_guard)
+                case Fighter::Status_Jump_Squat:
                 {
-                    dodgeBufferedTemp |= playerBit;
-                }
-                if (mechUtil::currAnimProgress(fighterIn) == 1.0f && dodgeBufferedTemp & playerBit)
-                {
-                    statusModule->changeStatusRequest(Fighter::Status_Escape_Air, moduleAccesser);
-                    dodgeBufferedTemp &= ~playerBit;
-                }
-            }
-
-            if (currStatus == Fighter::Status_Escape_Air)
-            {
-                float currAnimProgress = mechUtil::currAnimProgress(fighterIn);
-                int airdodgeTimer = workManageModule->getInt(airdodgeTimerVar);
-
-                if (currAnimProgress <= 0.25)
-                {
-                    soGroundModule* groundModule = moduleAccesser->m_enumerationStart->m_groundModule;
-                    Vec3f currPos;
-                    Vec2f* currPosCastAddr = (Vec2f*)(&currPos);
-                    *currPosCastAddr = groundModule->getDownPos(0);
-                    currPos.m_z = 0.0f;
-
-                    int lineIDOut;
-                    Vec3f hitPosOut;
-                    Vec3f normalVecOut;
-                    if (stRayCheck(&currPos, &searchVector, &lineIDOut, &hitPosOut, &normalVecOut, 1, 0, 1))
+                    if (moduleAccesser->m_enumerationStart->m_controllerModule->getTrigger().m_guard)
                     {
-                        float distanceFromGround = currPos.m_y - hitPosOut.m_y;
-                        OSReport_N("%sDistanceFromGround: %.3f\n", outputTag, distanceFromGround);
-                        soPostureModule* postureModule = moduleAccesser->m_enumerationStart->m_postureModule;
-                        groundModule->setCorrect(soGroundShapeImpl::Correct_None, 0);
-                        groundModule->setShapeSafePos((Vec2f*)&hitPosOut, 0);
-                        postureModule->setPos(&hitPosOut);
-                        groundModule->setCorrect(soGroundShapeImpl::Correct_Air, 0);
-                        groundModule->attachGround(0);
-                        groundModule->apply();
+                        dodgeBufferedTemp |= playerBit;
+                    }
+                    if (mechUtil::currAnimProgress(fighterIn) == 1.0f && dodgeBufferedTemp & playerBit)
+                    {
+                        statusModule->changeStatusRequest(Fighter::Status_Escape_Air, moduleAccesser);
+                        dodgeBufferedTemp &= ~playerBit;
+                    }
+                    break;
+                }
+                case Fighter::Status_Escape_Air:
+                {
+                    float currAnimProgress = mechUtil::currAnimProgress(fighterIn);
+                    int airdodgeTimer = workManageModule->getInt(airdodgeTimerVar);
+
+                    if (currAnimProgress <= 0.25)
+                    {
+                        soGroundModule* groundModule = moduleAccesser->m_enumerationStart->m_groundModule;
+                        Vec3f currPos;
+                        Vec2f* currPosCastAddr = (Vec2f*)(&currPos);
+                        *currPosCastAddr = groundModule->getDownPos(0);
+                        currPos.m_z = 0.0f;
+
+                        int lineIDOut;
+                        Vec3f hitPosOut;
+                        Vec3f normalVecOut;
+                        if (stRayCheck(&currPos, &searchVector, &lineIDOut, &hitPosOut, &normalVecOut, 1, 0, 1))
+                        {
+                            float distanceFromGround = currPos.m_y - hitPosOut.m_y;
+                            OSReport_N("%sDistanceFromGround: %.3f\n", outputTag, distanceFromGround);
+                            soPostureModule* postureModule = moduleAccesser->m_enumerationStart->m_postureModule;
+                            groundModule->setCorrect(soGroundShapeImpl::Correct_None, 0);
+                            groundModule->setShapeSafePos((Vec2f*)&hitPosOut, 0);
+                            postureModule->setPos(&hitPosOut);
+                            groundModule->setCorrect(soGroundShapeImpl::Correct_Air, 0);
+                            groundModule->attachGround(0);
+                            groundModule->apply();
+                        }
+                    }
+                    if (airdodgeTimer <= 0x00)
+                    {
+                        dodgeSpentTemp |= playerBit;
+                        airdodgeTimer--;
+                    }
+                    workManageModule->setInt(airdodgeTimer, airdodgeTimerVar);
+                    if (airdodgeTimer <= -0x06)
+                    {
+                        workManageModule->setInt(0, airdodgeTimerVar);
+                        statusModule->changeStatus(Fighter::Status_Fall_Aerial, moduleAccesser);
+                    }
+                    break;
+                }
+                case Fighter::Status_Attack:
+                {
+                    soMotionModule* motionModule = moduleAccesser->m_enumerationStart->m_motionModule;
+                    u32 currMotion = motionModule->getKind();
+                    if (mechUtil::currAnimProgress(fighterIn) >= 0.25
+                        && currMotion == Fighter::Motion_Attack_11 || currMotion == Fighter::Motion_Attack_12)
+                    {
+                        soTransitionModule* transitionModule = statusModule->m_transitionModule;
+                        transitionModule->enableTermGroup(0x4);
+                        transitionModule->unableTermAll(0x4);
+                        transitionModule->enableTerm(Fighter::Status_Transition_Term_Cont_Attack_S3, Fighter::Status_Transition_Term_Group_Chk_Ground_Attack);
+                        transitionModule->enableTerm(Fighter::Status_Transition_Term_Cont_Attack_Hi3, Fighter::Status_Transition_Term_Group_Chk_Ground_Attack);
+                        transitionModule->enableTerm(Fighter::Status_Transition_Term_Cont_Attack_Lw3, Fighter::Status_Transition_Term_Group_Chk_Ground_Attack);
+                    }
+                    break;
+                }
+                case Fighter::Status_FuraFura:
+                {
+                    statusModule->changeStatus(Fighter::Status_FuraFura_End, moduleAccesser);
+                    break;
+                }
+                case Fighter::Status_Wall_Jump:
+                {
+                    walljumpSpentTemp |= playerBit;
+                    break;
+                }
+                case Fighter::Status_Guard_On: case Fighter::Status_Guard:
+                {
+                    soControllerModule* controllerModule = moduleAccesser->m_enumerationStart->m_controllerModule;
+                    if (controllerModule->getTrigger().m_special)
+                    {
+                        soTransitionModule* transitionModule = statusModule->m_transitionModule;
+                        statusModule->changeStatusRequest(Fighter::Status_Guard_Off, moduleAccesser);
+                        parryBufferedTemp |= playerBit;
+                    }
+                    break;
+                }
+                case Fighter::Status_Guard_Off:
+                {
+                    soMotionModule* motionModule = moduleAccesser->m_enumerationStart->m_motionModule;
+                    soDamageModule* damageModule = moduleAccesser->m_enumerationStart->m_damageModule;
+                    if (parryBufferedTemp & playerBit)
+                    {
+                        motionModule->setRate(0.33f);
+                        moduleAccesser->m_enumerationStart->m_effectModule->req(ef_ptc_common_vertical_smoke_b, mechUtil::sbid_TransN,
+                            &mechUtil::zeroVec, &mechUtil::zeroVec, 1.0f, &mechUtil::zeroVec, &mechUtil::zeroVec, 0, 0);
+                        parryBufferedTemp &= ~playerBit;
+                        damageModule->setNoReactionModeStatus(500.0f, -1.0f, 2);
+                        damageModule->setDamageMul(0.0f);
+                        workManageModule->onFlag(parryActiveBit);
+                        workManageModule->setInt(0x00, 0x20000003);
+                        workManageModule->setInt(0xFF, 0x20000005);
+                    }
+                    if (workManageModule->isFlag(parryActiveBit) && mechUtil::currAnimProgress(fighterIn) >= 0.33f)
+                    {
+                        damageModule->resetNoReactionModeStatus();
+                        damageModule->setDamageMul(1.0f);
+                        workManageModule->offFlag(parryActiveBit);
+                        if (workManageModule->isFlag(parrySuccessBit))
+                        {
+                            workManageModule->setInt(0x5, 0x20000005);
+                        }
+                        else
+                        {
+                            motionModule->setRate(0.2f);
+                        }
+                        break;
                     }
                 }
-                if (airdodgeTimer <= 0x00)
+                case Fighter::Status_Fall_Special:
                 {
-                    dodgeSpentTemp |= playerBit;
-                    airdodgeTimer--;
-                }
-                workManageModule->setInt(airdodgeTimer, airdodgeTimerVar);
-                if (airdodgeTimer <= -0x06)
-                {
-                    workManageModule->setInt(0, airdodgeTimerVar);
-                    statusModule->changeStatus(Fighter::Status_Fall_Aerial, moduleAccesser);
-                }
-            }
-            else if (currStatus == Fighter::Status_Attack)
-            {
-                soMotionModule* motionModule = moduleAccesser->m_enumerationStart->m_motionModule;
-                u32 currMotion = motionModule->getKind();
-                if (mechUtil::currAnimProgress(fighterIn) >= 0.25 
-                    && currMotion == Fighter::Motion_Attack_11 || currMotion == Fighter::Motion_Attack_12)
-                {
-                    soTransitionModule* transitionModule = statusModule->m_transitionModule;
-                    transitionModule->enableTermGroup(0x4);
-                    transitionModule->unableTermAll(0x4);
-                    transitionModule->enableTerm(Fighter::Status_Transition_Term_Cont_Attack_S3, Fighter::Status_Transition_Term_Group_Chk_Ground_Attack);
-                    transitionModule->enableTerm(Fighter::Status_Transition_Term_Cont_Attack_Hi3, Fighter::Status_Transition_Term_Group_Chk_Ground_Attack);
-                    transitionModule->enableTerm(Fighter::Status_Transition_Term_Cont_Attack_Lw3, Fighter::Status_Transition_Term_Group_Chk_Ground_Attack);
-                }
-            }
-            else if (currStatus == Fighter::Status_FuraFura)
-            {
-                statusModule->changeStatus(Fighter::Status_FuraFura_End, moduleAccesser);
-            }
-            else if (currStatus == Fighter::Status_Wall_Jump)
-            {
-                walljumpSpentTemp |= playerBit;
-            }
-            else if (currStatus == Fighter::Status_Guard_On || currStatus == Fighter::Status_Guard)
-            {
-                soControllerModule* controllerModule = moduleAccesser->m_enumerationStart->m_controllerModule;
-                if (controllerModule->getTrigger().m_special)
-                {
-                    soTransitionModule* transitionModule = statusModule->m_transitionModule;
-                    statusModule->changeStatusRequest(Fighter::Status_Guard_Off, moduleAccesser);
-                    parryBufferedTemp |= playerBit;
-                }
-            }
-            else if (currStatus == Fighter::Status_Guard_Off)
-            {
-                soMotionModule* motionModule = moduleAccesser->m_enumerationStart->m_motionModule;
-                soDamageModule* damageModule = moduleAccesser->m_enumerationStart->m_damageModule;
-                if (parryBufferedTemp & playerBit)
-                {
-                    motionModule->setRate(0.33f);
-                    moduleAccesser->m_enumerationStart->m_effectModule->req(ef_ptc_common_vertical_smoke_b, mechUtil::sbid_TransN,
-                        &mechUtil::zeroVec, &mechUtil::zeroVec, 1.0f, &mechUtil::zeroVec, &mechUtil::zeroVec, 0, 0);
-                    parryBufferedTemp &= ~playerBit;
-                    damageModule->setNoReactionModeStatus(500.0f, -1.0f, 2);
-                    damageModule->setDamageMul(0.0f);
-                    workManageModule->onFlag(parryActiveBit);
-                    workManageModule->setInt(0x00, 0x20000003);
-                    workManageModule->setInt(0xFF, 0x20000005);
-                }
-                if (workManageModule->isFlag(parryActiveBit) && mechUtil::currAnimProgress(fighterIn) >= 0.33f)
-                {
-                    damageModule->resetNoReactionModeStatus();
-                    damageModule->setDamageMul(1.0f);
-                    workManageModule->offFlag(parryActiveBit);
-                    if (workManageModule->isFlag(parrySuccessBit))
+                    if ((walljumpSpent & playerBit) == 0)
                     {
-                        workManageModule->setInt(0x5, 0x20000005);
+                        statusModule->enableTransitionTermGroup(Fighter::Status_Transition_Term_Group_Chk_Air_Wall_Jump);
                     }
-                    else
+                    break;
+                }
+                case Fighter::Status_Dash:
+                {
+                    if (moduleAccesser->m_enumerationStart->m_motionModule->getFrame() < 3.0f)
                     {
-                        motionModule->setRate(0.2f);
+                        float stickAbs = ftValueAccesser::getVariableFloat(moduleAccesser, ftValueAccesser::Variable_Float_Controller_Stick_X_Abs, 0);
+                        if (stickAbs < 0.2f)
+                        {
+                            soKineticModule* kineticModule = moduleAccesser->m_enumerationStart->m_kineticModule;
+                            soInstanceAttribute energyMask = { 0xFFFF };
+                            Vec3f currSpeed = kineticModule->getSumSpeed3f(&energyMask);
+                            currSpeed.m_x *= moduleAccesser->m_enumerationStart->m_postureModule->getLr();
+                            statusModule->changeStatus(Fighter::Status_Wait, moduleAccesser);
+                            kineticModule->addSpeed(&currSpeed, moduleAccesser);
+                        }
                     }
+                    break;
                 }
             }
 
@@ -179,20 +213,10 @@ namespace squatDodge
                     {
                         statusModule->unableTransitionTermGroup(Fighter::Status_Transition_Term_Group_Chk_Air_Wall_Jump);
                     }
-                }
-            }
-
-            if (currStatus == Fighter::Status_Dash && moduleAccesser->m_enumerationStart->m_motionModule->getFrame() < 3.0f)
-            {
-                float stickAbs = ftValueAccesser::getVariableFloat(moduleAccesser, ftValueAccesser::Variable_Float_Controller_Stick_X_Abs, 0);
-                if (stickAbs < 0.2f)
-                {
-                    soKineticModule* kineticModule = moduleAccesser->m_enumerationStart->m_kineticModule;
-                    soInstanceAttribute energyMask = { 0xFFFF };
-                    Vec3f currSpeed = kineticModule->getSumSpeed3f(&energyMask);
-                    currSpeed.m_x *= moduleAccesser->m_enumerationStart->m_postureModule->getLr() * 0.75f;
-                    statusModule->changeStatus(Fighter::Status_Wait, moduleAccesser);
-                    kineticModule->addSpeed(&currSpeed, moduleAccesser);
+                    else if (currStatus >= Fighter::Status_Test_Motion && mechUtil::currAnimProgress(fighterIn) > 0.75)
+                    {
+                        statusModule->enableTransitionTermGroup(Fighter::Status_Transition_Term_Group_Chk_Air_Wall_Jump);
+                    }
                 }
             }
 
