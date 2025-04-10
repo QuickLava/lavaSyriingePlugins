ac8ef0c55e9162b5b308be00d6838984285a5e54
QuickLava <fastmovingvolcanomatter@gmail.com>
2025-04-09 20:49:48 -0700

    Reimplemented RastVar Whitelist System
    Major overhaul! I intend to implement support for just about every PSA Command, and practically speaking having to search through hundreds of entries in the whitelist on every command dispatch seemed like it may actually start to weigh on performance at some point, so I've refactored the system to organize commands by group. Should help performance quite a lot, plus knowing what module each command belongs to meant being able to free up another byte in the whitelist entries themselves, allowing us to support *48* arguments on a single instruction, giving us *juuuust* enough room to cover even the very longest commands in vanilla.
    
    If it comes to be that after implementing everything performance is still bad, we could pretty cheaply add a small bit in our constructor to add up the size values in each commandGroup, at which point we could change the group array so that we can just index in with the Module ID we pull from a command. Wish there was a clean way to do that at compile time, but without constexpr it seems like our options are limited unfortunately.

--- lavaPSAExtensions/include/rasterizeVars.h
+++ lavaPSAExtensions/include/rasterizeVars.h
@@ -5,6 +5,83 @@
 
 namespace rasterizeVars
 {
+    enum cmdModuleID
+    {
+        cmid_SYSTEM = 0x00,
+        cmid_01,
+        cmid_STATUS,
+        cmid_MODEL,
+        cmid_MOTION,
+        cmid_POSTURE,
+        cmid_COLLISION,
+        cmid_CONTROLLER,
+        cmid_GROUND,
+        cmid_SITUATION,
+        cmid_SOUND,
+        cmid_VISIBILITY,
+        cmid_CHAR_SPECIFIC,
+        cmid_ANIMCMD,
+        cmid_KINETIC,
+        cmid_LINK,
+        cmid_ARTICLE,
+        cmid_EFFECT,
+        cmid_WORK_MANAGE,
+        cmid_COMBO,
+        cmid_AREA,
+        cmid_TERRITORY,
+        cmid_SEARCH,
+        cmid_PHYSICS,
+        cmid_SLOPE,
+        cmid_SHADOW,
+        cmid_CAMERA,
+        cmid_1B,
+        cmid_STOP,
+        cmid_SHAKE,
+        cmid_DAMAGE,
+        cmid_ITEM,
+        cmid_TURN,
+        cmid_COLOR_BLEND,
+        cmid_TEAM,
+        cmid_SLOW,
+        cmid__COUNT,
+    };
+
+    struct cmdWhitelistEntry
+    {
+        u8 m_code; // AnimCMD Instruction Code ID
+        u8 m_bankIndices[3]; // typeLibraryIndices entry identifying the typeBank for Args 0-15 and 16-32 respectively.
+    };
+    struct cmdWhitelistCmdGroup
+    {
+        u8 m_moduleID;
+        u8 m_entryCount;
+    };
+    struct cmdWhitelist
+    {
+        enum supportedCommandGroups
+        {
+            scg_SYSTEM,
+            scg_MODEL,
+            scg_MOTION,
+            scg_POSTURE,
+            scg_COLLISION,
+            scg_KINETIC,
+            scg_EFFECT,
+            scg_COLOR_BLEND,
+            scg__COUNT,
+        };
+
+        static const cmdWhitelistEntry m_allowedCommands_System[];
+        static const cmdWhitelistEntry m_allowedCommands_Model[];
+        static const cmdWhitelistEntry m_allowedCommands_Motion[];
+        static const cmdWhitelistEntry m_allowedCommands_Posture[];
+        static const cmdWhitelistEntry m_allowedCommands_Collision[];
+        static const cmdWhitelistEntry m_allowedCommands_Kinetic[];
+        static const cmdWhitelistEntry m_allowedCommands_Effect[];
+        static const cmdWhitelistEntry m_allowedCommands_ColorBlend[];
+        static const cmdWhitelistCmdGroup m_commandGroups[scg__COUNT];
+    };
+
     void registerHooks();
 }
 
--- lavaPSAExtensions/source/rasterizeVars.cpp
+++ lavaPSAExtensions/source/rasterizeVars.cpp
@@ -9,7 +9,7 @@ namespace rasterizeVars
 {
     const char outputTag[] = "[rasterizeVars] ";
     const char rasterizeFmtStr[] = "%sRasterized Arg%2d in %08X: %08X -> %08X (%s)\n";
-    const u32 maxArgumentCount = 0x20;
+    const u32 maxArgumentCount = 0x30;
 
     enum argType
     {
@@ -153,117 +153,172 @@ namespace rasterizeVars
     };
 #pragma c99 off
 
-    struct cmdWhitelistEntry
+    // Group 0x00: System Commands (Flow of Execution)
+    const cmdWhitelistEntry cmdWhitelist::m_allowedCommands_System[] =
     {
-        u8 m_module; // AnimCMD Instruction Module ID
-        u8 m_code; // AnimCMD Instruction Code ID
-        u8 m_bankIndices[2]; // typeLibraryIndices entry identifying the typeBank for Args 0-15 and 16-32 respectively.
-
-        u16 getFullID() const
-        {
-            return *(u16*)this;
-        }
+        { 0x01, tli_1 },                               // [00010100] Synchronous Timer
+        { 0x02, tli_1 },                               // [00020100] Asynchronous Timer
+        { 0x04, tli_0 },                               // [00040100] Set Loop
+        { 0x07, tli_0 },                               // [00070100] Subroutine
+        { 0x09, tli_0 },                               // [00090100] Goto
     };
-    const cmdWhitelistEntry allowedCommands[] =
+    // Group 0x03: soModelModule
+    const cmdWhitelistEntry cmdWhitelist::m_allowedCommands_Model[] =
     {
-        // Bone Commands
-        { 0x03, 0x01, tli_0111 },                            // [03010400] Set Bone Rotation (XYZ)
-        { 0x03, 0x02, tli_01 },                              // [03020200] Set Bone Rotation (X)
-        { 0x03, 0x03, tli_01 },                              // [03030200] Set Bone Rotation (Y)
-        { 0x03, 0x04, tli_01 },                              // [03040200] Set Bone Rotation (Z)
-        { 0x03, 0x06, tli_0111 },                            // [03060400] Set Bone Scale (XYZ)
-        { 0x03, 0x07, tli_01 },                              // [03070200] Set Bone Scale (X)
-        { 0x03, 0x08, tli_01 },                              // [03080200] Set Bone Scale (Y)
-        { 0x03, 0x09, tli_01 },                              // [03090200] Set Bone Scale (Z)
-        { 0x03, 0x0B, tli_0111 },                            // [030B0400] Set Bone Translation (XYZ)
-        { 0x03, 0x0C, tli_01 },                              // [030C0200] Set Bone Translation (X)
-        { 0x03, 0x0D, tli_01 },                              // [030D0200] Set Bone Translation (Y)
-        { 0x03, 0x0E, tli_01 },                              // [030E0200] Set Bone Translation (Z)
-
-        // Anim Override Commands
-        { 0x04, 0x0E, tli_01 },                              // [040E0200] Set Bone Motion Override Animation Frame
-        { 0x04, 0x0F, tli_01 },                              // [040F0200] Set Bone Motion Override Frame Speed Modifier
-
-        // Char Pos/Rot Commands
-        { 0x05, 0x05, tli_1 },                               // [05050100] Change Model Size
-        { 0x05, 0x06, tli_111 },                             // [05060300] Rotate Character Model
-        { 0x05, 0x09, tli_111 },                             // [05090300] Set Character Position
-        { 0x05, 0x0A, tli_111 },                             // [050A0300] Set Character Position 2
-        { 0x05, 0x0B, tli_111 },                             // [050B0300] Set Character Position (Relative)
-
-        // Hitbox Commands
-        { 0x06, 0x00, tli_000001111111000 },                 // [06000D00] Offensive Collision
-        { 0x06, 0x01, tli_01 },                              // [06010200] Change Hitbox Damage
-        { 0x06, 0x02, tli_01 },                              // [06020200] Change Hitbox Size
-        { 0x06, 0x0A, tli_00111100 },                        // [060A0800] Catch Collision
-        { 0x06, 0x0E, tli_0000000011100033, tli_0 },         // [060E1100] Throw Attack Collision
-        { 0x06, 0x0F, tli_00555 },                           // [060F0500] Throw Collision
-        { 0x06, 0x10, tli_0001111000300303, tli_0 },         // [06101100] Inert Collision
-        { 0x06, 0x15, tli_000001111111000 },                 // [06150F00] Special Offensive Collision
-        { 0x06, 0x17, tli_000 },                             // [06170300] Defensive Collision
-        { 0x06, 0x1B, tli_00111 },                           // [061B0500] Move Hitbox
-        { 0x06, 0x1E, tli_030 },                             // [061E0300] Defensive Collision: Flip Toggle
-        { 0x06, 0x24, tli_0001111111111310 },                // [06241000] Generate Defensive Collision Bubble
-        { 0x06, 0x2B, tli_000001111111000 },                 // [062B0D00] Thrown Collision
-        { 0x06, 0x2C, tli_000001111111000 },                 // [062C0F00] Special Collateral Collision
-
-        // Kinetic Module Commands
-        { 0x0E, 0x02, tli_0 },                               // [0E020100] Prevent Horizontal Gravity
-        { 0x0E, 0x03, tli_0 },                               // [0E030100] ???
-        { 0x0E, 0x04, tli_0 },                               // [0E040100] Prevent Horizontal Gravity
-        { 0x0E, 0x05, tli_0 },                               // [0E050100] Set Air/Ground Article/Item
-        { 0x0E, 0x06, tli_0 },                               // [0E060100] Disallow Certain Movements
-        { 0x0E, 0x07, tli_0 },                               // [0E070100] Reallow Certain Movements
-        { 0x0E, 0x08, tli_1133 },                            // [0E080400] Set Momentum, Set/Add Momentum
-
-        // Graphics Commands
-        { 0x11, 0x00, tli_0011111111111113 },                // [11001000] Graphic Effect
-        { 0x11, 0x01, tli_0011111113 },                      // [11010A00] Graphic Effect (Attached)
-        { 0x11, 0x02, tli_0011111113 },                      // [11020A00] Graphic Effect (Attached 2)
-        { 0x11, 0x03, tli_0001110111300111, tli_1111001 },   // [11031400] Sword Glow
-        { 0x11, 0x04, tli_0001110111300111, tli_1111001 },   // [11041700] Sword/Hammer Glow
-        { 0x11, 0x15, tli_033},                              // [11150300] Terminate Graphic Effect
-        { 0x11, 0x19, tli_0011111113 },                      // [11190A00] Graphic Effect (Attached 19)
-        { 0x11, 0x1A, tli_0011111111111113 },                // [111A1000] Graphic Effect (Stepping)
-        { 0x11, 0x1B, tli_0011111111111113 },                // [111B1000] Graphic Effect (Landing)
-        { 0x11, 0x1C, tli_0011111111111113 },                // [111C1000] Graphic Effect (Tumbling)
-
-        // Flash Overlay Commands
-        { 0x21, 0x01, tli_0000 },                            // [21010400] Flash Overlay Effect
-        { 0x21, 0x02, tli_00000 },                           // [21020500] Change Flash Overlay Color
-        { 0x21, 0x05, tli_000011 },                          // [21050600] Flash Light Effect
-        { 0x21, 0x07, tli_00000 },                           // [21070500] Change Flash Light Color
+        { 0x01, tli_0111 },                            // [03010400] Set Bone Rotation (XYZ)
+        { 0x02, tli_01 },                              // [03020200] Set Bone Rotation (X)
+        { 0x03, tli_01 },                              // [03030200] Set Bone Rotation (Y)
+        { 0x04, tli_01 },                              // [03040200] Set Bone Rotation (Z)
+        { 0x06, tli_0111 },                            // [03060400] Set Bone Scale (XYZ)
+        { 0x07, tli_01 },                              // [03070200] Set Bone Scale (X)
+        { 0x08, tli_01 },                              // [03080200] Set Bone Scale (Y)
+        { 0x09, tli_01 },                              // [03090200] Set Bone Scale (Z)
+        { 0x0B, tli_0111 },                            // [030B0400] Set Bone Translation (XYZ)
+        { 0x0C, tli_01 },                              // [030C0200] Set Bone Translation (X)
+        { 0x0D, tli_01 },                              // [030D0200] Set Bone Translation (Y)
+        { 0x0E, tli_01 },                              // [030E0200] Set Bone Translation (Z)
     };
-    const u32 allowedCommandCount = sizeof(allowedCommands) / sizeof(cmdWhitelistEntry);
+    // Group 0x04: soMotionModule
+    const cmdWhitelistEntry cmdWhitelist::m_allowedCommands_Motion[] =
+    {
+        { 0x0E, tli_01 },                              // [040E0200] Set Bone Motion Override Animation Frame
+        { 0x0F, tli_01 },                              // [040F0200] Set Bone Motion Override Frame Speed Modifier
+    };
+    // Group 0x05: soPostureModule
+    const cmdWhitelistEntry cmdWhitelist::m_allowedCommands_Posture[] =
+    {
+        { 0x05, tli_1 },                               // [05050100] Change Model Size
+        { 0x06, tli_111 },                             // [05060300] Rotate Character Model
+        { 0x09, tli_111 },                             // [05090300] Set Character Position
+        { 0x0A, tli_111 },                             // [050A0300] Set Character Position 2
+        { 0x0B, tli_111 },                             // [050B0300] Set Character Position (Relative)
+    };
+    // Group 0x06: soCollision_______Modules
+    const cmdWhitelistEntry cmdWhitelist::m_allowedCommands_Collision[] =
+    {
+        { 0x00, tli_000001111111000 },                 // [06000D00] Offensive Collision
+        { 0x01, tli_01 },                              // [06010200] Change Hitbox Damage
+        { 0x02, tli_01 },                              // [06020200] Change Hitbox Size
+        { 0x0A, tli_00111100 },                        // [060A0800] Catch Collision
+        { 0x0E, tli_0000000011100033, tli_0 },         // [060E1100] Throw Attack Collision
+        { 0x0F, tli_00555 },                           // [060F0500] Throw Collision
+        { 0x10, tli_0001111000300303, tli_0 },         // [06101100] Inert Collision
+        { 0x15, tli_000001111111000 },                 // [06150F00] Special Offensive Collision
+        { 0x17, tli_000 },                             // [06170300] Defensive Collision
+        { 0x1B, tli_00111 },                           // [061B0500] Move Hitbox
+        { 0x1E, tli_030 },                             // [061E0300] Defensive Collision: Flip Toggle
+        { 0x24, tli_0001111111111310 },                // [06241000] Generate Defensive Collision Bubble
+        { 0x2B, tli_000001111111000 },                 // [062B0D00] Thrown Collision
+        { 0x2C, tli_000001111111000 },                 // [062C0F00] Special Collateral Collision
+    };
+    // Group 0x0E: soKineticModule
+    const cmdWhitelistEntry cmdWhitelist::m_allowedCommands_Kinetic[] =
+    {
+        { 0x02, tli_0 },                               // [0E020100] Prevent Horizontal Gravity
+        { 0x03, tli_0 },                               // [0E030100] ???
+        { 0x04, tli_0 },                               // [0E040100] Prevent Horizontal Gravity
+        { 0x05, tli_0 },                               // [0E050100] Set Air/Ground Article/Item
+        { 0x06, tli_0 },                               // [0E060100] Disallow Certain Movements
+        { 0x07, tli_0 },                               // [0E070100] Reallow Certain Movements
+        { 0x08, tli_1133 },                            // [0E080400] Set Momentum, Set/Add Momentum
+    };
+    // Group 0x11: soEffectModule
+    const cmdWhitelistEntry cmdWhitelist::m_allowedCommands_Effect[] =
+    {
+        { 0x00, tli_0011111111111113 },                // [11001000] Graphic Effect
+        { 0x01, tli_0011111113 },                      // [11010A00] Graphic Effect (Attached)
+        { 0x02, tli_0011111113 },                      // [11020A00] Graphic Effect (Attached 2)
+        { 0x03, tli_0001110111300111, tli_1111001 },   // [11031400] Sword Glow
+        { 0x04, tli_0001110111300111, tli_1111001 },   // [11041700] Sword/Hammer Glow
+        { 0x15, tli_033},                              // [11150300] Terminate Graphic Effect
+        { 0x19, tli_0011111113 },                      // [11190A00] Graphic Effect (Attached 19)
+        { 0x1A, tli_0011111111111113 },                // [111A1000] Graphic Effect (Stepping)
+        { 0x1B, tli_0011111111111113 },                // [111B1000] Graphic Effect (Landing)
+        { 0x1C, tli_0011111111111113 },                // [111C1000] Graphic Effect (Tumbling)
+    };
+    // Group 0x21: soColorBlendModule
+    const cmdWhitelistEntry cmdWhitelist::m_allowedCommands_ColorBlend[] =
+    {
+        { 0x01, tli_0000 },                            // [21010400] Flash Overlay Effect
+        { 0x02, tli_00000 },                           // [21020500] Change Flash Overlay Color
+        { 0x05, tli_000011 },                          // [21050600] Flash Light Effect
+        { 0x07, tli_00000 },                           // [21070500] Change Flash Light Color
+    };
+#pragma c99 on
+#define COUNT_OF(x) ((sizeof(x)/sizeof(0[x])) / ((size_t)(!(sizeof(x) % sizeof(0[x])))))
+#define COUNT_OF_ALIGNED(x) COUNT_OF(x) + (COUNT_OF(x) % 2)
+    const cmdWhitelistCmdGroup cmdWhitelist::m_commandGroups[] =
+    {
+        [cmdWhitelist::scg_SYSTEM] = {cmid_SYSTEM, COUNT_OF_ALIGNED(cmdWhitelist::m_allowedCommands_System)},
+        [cmdWhitelist::scg_MODEL] = {cmid_MODEL, COUNT_OF_ALIGNED(cmdWhitelist::m_allowedCommands_Model)},
+        [cmdWhitelist::scg_MOTION] = {cmid_MOTION, COUNT_OF_ALIGNED(cmdWhitelist::m_allowedCommands_Motion)},
+        [cmdWhitelist::scg_POSTURE] = {cmid_POSTURE, COUNT_OF_ALIGNED(cmdWhitelist::m_allowedCommands_Posture)},
+        [cmdWhitelist::scg_COLLISION] = {cmid_COLLISION, COUNT_OF_ALIGNED(cmdWhitelist::m_allowedCommands_Collision)},
+        [cmdWhitelist::scg_KINETIC] = {cmid_KINETIC, COUNT_OF_ALIGNED(cmdWhitelist::m_allowedCommands_Kinetic)},
+        [cmdWhitelist::scg_EFFECT] = {cmid_EFFECT, COUNT_OF_ALIGNED(cmdWhitelist::m_allowedCommands_Effect)},
+        [cmdWhitelist::scg_COLOR_BLEND] = {cmid_COLOR_BLEND, COUNT_OF_ALIGNED(cmdWhitelist::m_allowedCommands_ColorBlend)},
+    };
+    const u32 commandGroupCount = COUNT_OF(cmdWhitelist::m_commandGroups);
+#pragma c99 off
 
     void rasterizeVariables(acAnimCmdImpl* cmdIn, soModuleAccesser* accesserIn, soAnimCmd* proxyIn, soAnimCmdArgument* argBufferIn)
     {
         soAnimCmd* rawCommandPtr = ((soAnimCmd**)cmdIn)[2];
         u32 commandSignature = *((u32*)rawCommandPtr);
-        const u16 commandFullID = commandSignature >> 0x10;
-
 
         if (rawCommandPtr->m_argCount <= 0x00 || rawCommandPtr->m_argCount > maxArgumentCount) return;
 
-        bool cmdAllowed = 0;
-        const cmdWhitelistEntry* currWhitelistEntry = allowedCommands - 1;
-        for (int i = 0; i < allowedCommandCount; i++)
+        bool groupAllowed = 0;
+        u32 entriesToTargetVector = 0x00;
+        const cmdWhitelistCmdGroup* currGroup = cmdWhitelist::m_commandGroups - 1;
+        for (u32 i = 0, targetModuleID = (commandSignature >> 0x18) & 0xFF; i < commandGroupCount; i++)
         {
-            currWhitelistEntry++;
-            if (currWhitelistEntry->getFullID() == commandFullID)
+            currGroup++;
+            u32 currModuleID = currGroup->m_moduleID;
+            if (currModuleID == targetModuleID)
+            {
+                groupAllowed = 1;
+                break;
+            }
+            else
+            {
+                entriesToTargetVector += currGroup->m_entryCount;
+                if (currModuleID > targetModuleID)
+                {
+                    break;
+                }
+            }
+        }
+        if (!groupAllowed) return;
+        
+        bool cmdAllowed = 0;
+        const u32 targetCmdListCount = currGroup->m_entryCount;
+        const cmdWhitelistEntry* currWhitelistEntry = cmdWhitelist::m_allowedCommands_System + entriesToTargetVector;
+        for (u32 i = 0, targetCommandID = (commandSignature >> 0x10) & 0xFF; i < targetCmdListCount; i++)
+        {
+            u32 currCommandID = currWhitelistEntry->m_code;
+            if (currCommandID == targetCommandID)
             {
                 cmdAllowed = 1;
                 break;
             }
+            else
+            {
+                currWhitelistEntry++;
+                if (currCommandID > targetCommandID)
+                {
+                    break;
+                }
+            }
         }
         if (!cmdAllowed) return;
-        
+
         soAnimCmdArgument* currArg = rawCommandPtr->m_args;
         soAnimCmdArgument* currBufArg = argBufferIn;
-
-        for (int i = 0; i < rawCommandPtr->m_argCount; i++)
+        const u32 currArgCount = rawCommandPtr->m_argCount;
+        for (u32 i = 0; i < currArgCount; i++)
         {
-            const argTypeBank* targetFlagBank = &typeBankLibrary[currWhitelistEntry->m_bankIndices[(i >> 0x4) & 0b1]];
+            const argTypeBank* targetFlagBank = &typeBankLibrary[currWhitelistEntry->m_bankIndices[(i >> 0x4) & 0b11]];
             argType currArgType = targetFlagBank->getArgType(i);
             if ((currArgType != at_NULL) && currArg->m_varType == AnimCmd_Arg_Type_Variable)
             {
