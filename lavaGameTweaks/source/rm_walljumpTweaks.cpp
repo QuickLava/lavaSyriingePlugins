d4c81e3bb314166c24e44b323ea84771ff689260
QuickLava <fastmovingvolcanomatter@gmail.com>
2025-09-06 21:52:58 -0700

    Reimp'd Once-Per-Airtime Walljump Mechanic
    Upon thinking further about this, I realized I'd actually interpreted the mechanic incorrectly: it isn't so much that you can only walljump once per airtime, it's that you're required to walljump in *opposing directions* (which just happens to be near-impossible in one airtime on most legal stages)! With that in mind, I've rewritten most of how that's handled, and in doing so allowed characters to walljump back and forth between opposing walls again like they can in Rivals. Code Menu option and internal labelling changed to reflect this as well.

--- lavaGameTweaks/source/rm_walljumpTweaks.cpp
+++ lavaGameTweaks/source/rm_walljumpTweaks.cpp
@@ -11,39 +11,93 @@ namespace rmWalljumpTweaks
     enum playerFlags
     {
         pf_WalljumpAllowedNative = 0x00,
-        pf_WallJumpSpent,
         pf_WalljumpOutOfSpecialEnabled,
         pf__COUNT
     };
     u8 perPlayerFlags[pf__COUNT];
-
-    void tryExtendedWalljump(Fighter* fighterIn)
+    enum walljumpSituation
     {
+        wt_INVALID = -1,
+        wt_NONE = 0,
+        wt_LEFT,
+        wt_RIGHT,
+        wt__COUNT
+    };
+    u8 perPlayerPrevWalljump[fighterHooks::maxFighterCount];
+    walljumpSituation decideWalljumpSituation(Fighter* fighterIn)
+    {
+        bool situationValid = 0;
+        walljumpSituation currWalljumpSituation = wt_NONE;
+
+        // If we're a fighter in a valid slot, in the air, on a character that's allowed to walljump...
         u32 fighterPlayerNo = fighterHooks::getFighterPlayerNo(fighterIn);
         soModuleAccesser* moduleAccesser = fighterIn->m_moduleAccesser;
-        soModuleEnumeration* moduleEnum = moduleAccesser->m_enumerationStart;
-        // If this fighter is in a valid slot, can walljump, *and* their current state allows for it..
-        if (fighterPlayerNo < fighterHooks::maxFighterCount
-            && ftValueAccesser::getConstantInt(moduleAccesser, ftValueAccesser::Info_Param_Int_Wall_Jump_Type, 0)
-            && moduleEnum->m_statusModule->isEnableTransitionTermGroup(Fighter::Status_Transition_Term_Group_Chk_Air_Wall_Jump))
+        if (fighterPlayerNo < fighterHooks::maxFighterCount 
+            && moduleAccesser->m_enumerationStart->m_situationModule->getKind() == Situation_Air
+            && ftValueAccesser::getConstantInt(moduleAccesser, ftValueAccesser::Info_Param_Int_Wall_Jump_Type, 0))
         {
-            // ... then check that we're touching a wall.
-            soGroundModule* groundModule = moduleEnum->m_groundModule;
-            // Initialize final direction to 0.0, to indicate no successful jump.
-            float finalDir = 0.0f;
+            // ... process validity of current walljump situation!
+            walljumpSituation prevWalljumpSituation = (walljumpSituation)perPlayerPrevWalljump[fighterPlayerNo];
+            soGroundModule* groundModule = moduleAccesser->m_enumerationStart->m_groundModule;
             // If we're touching a wall to our left...
             if (groundModule->isTouch(2, 0))
             {
-                // ... then final direction is 1.0f (right).
-                finalDir = 1.0f;
+                // ... then we can currently jump right.
+                currWalljumpSituation = wt_RIGHT;
             }
             // If instead we're touching to our right...
             else if (groundModule->isTouch(4, 0))
             {
-                // ... final direction is -1.0f (left).
-                finalDir = -1.0f;
+                // ... then we can currently jump left.
+                currWalljumpSituation = wt_LEFT;
+            }
+            // Our current walljump at base is valid if we're touching a wall.
+            situationValid = currWalljumpSituation >= wt_NONE;
+
+            // If we're touching a wall, but the direction restriction is active...
+            if ((currWalljumpSituation > wt_NONE) && mechHub::getPassiveMechanicEnabled(fighterPlayerNo, mechHub::pmid_WALLJUMP_SAME_DIR_RESTRICTION))
+            {
+                // ... our current walljump situation is valid if current situation would have us jump in a different direction than we did last.
+                situationValid = currWalljumpSituation != prevWalljumpSituation;
+                // If current situation is invalid...
+                if (!situationValid)
+                {
+                    // ... then report that we're within the lockout still!
+                    OSReport_N("%sDirection Restriction Active: Situation %d -> %d\N", outputTag, prevWalljumpSituation, currWalljumpSituation);
+                }
             }
 
+            // If we're still valid, and we have button walljumping enabled.
+            if (situationValid && mechHub::getPassiveMechanicEnabled(fighterPlayerNo, mechHub::pmid_WALLJUMP_BUTTON))
+            {
+                // ... we need to also check how many frames it's been since we dropped from ledge.
+                u32 framesSinceLedgeGrab =
+                    ftValueAccesser::getConstantInt(moduleAccesser, ftValueAccesser::Common_Param_Int_Cliff_No_Catch_Frame, 0)
+                    - moduleAccesser->m_enumerationStart->m_workManageModule->getInt(Fighter::Instance_Work_Int_Cliff_No_Catch_Frame);
+                // Our situation is valid if we're out of the lockout window.
+                situationValid = framesSinceLedgeGrab >= 8;
+                // If it's invalid...
+                if (!situationValid)
+                {
+                    // ... then report that we're within the lockout stil!
+                    OSReport_N("Lockout Active\N", outputTag);
+                }
+            }
+        }
+
+        return situationValid ? currWalljumpSituation : wt_INVALID;
+    }
+    void tryButtonWalljump(Fighter* fighterIn)
+    {
+        // If this fighter is in a valid slot, can walljump, *and* their current state allows for it..
+        walljumpSituation currWalljumpSituation = decideWalljumpSituation(fighterIn);
+        if (currWalljumpSituation != wt_NONE)
+        {
+            soModuleAccesser* moduleAccesser = fighterIn->m_moduleAccesser;
+            soModuleEnumeration* moduleEnum = moduleAccesser->m_enumerationStart;
+
+            // Set finalDir based on the direction reported by the validation function.
+            float finalDir = (currWalljumpSituation == wt_RIGHT) ? 1.0f : -1.0f;
             // Finally get the absolute x position of the control stick.
             float stickXPos = ftValueAccesser::getVariableFloat(moduleAccesser, ftValueAccesser::Var_Float_Controller_Stick_X, 0);
             // If the stick is being held left or right, multiply finalDir into the current stick x position. If the result is less than 0.0f,
@@ -77,7 +131,6 @@ namespace rmWalljumpTweaks
 
             const u32 playerBit = 1 << fighterPlayerNo;
             u32 walljumpNativeTemp = perPlayerFlags[pf_WalljumpAllowedNative];
-            u32 walljumpSpentTemp = perPlayerFlags[pf_WallJumpSpent];
             u32 specialWalljumpTemp = perPlayerFlags[pf_WalljumpOutOfSpecialEnabled];
 
             u32 currStatus = statusModule->getStatusKind();
@@ -86,83 +139,63 @@ namespace rmWalljumpTweaks
             u32 currSituation = moduleAccesser->m_enumerationStart->m_situationModule->getKind();
             if (currSituation == Situation_Air)
             {
-                // ... and Walljump out of Special Fall is enabled...
-                if (mechHub::getPassiveMechanicEnabled(fighterPlayerNo, mechHub::pmid_WALLJUMP_FROM_SPECIAL))
+                if (moduleAccesser->m_enumerationStart->m_collisionAttackModule->isInflict())
                 {
-                    // ... check if we're currently in a special without the walljump flag set.
-                    if (currStatus > Fighter::Status_Test_Motion && (specialWalljumpTemp & playerBit) == 0)
+                    perPlayerPrevWalljump[fighterPlayerNo] = wt_NONE;
+                    OSReport_N("%s[P%d] Walljump Restored after Attack!\n", outputTag, fighterPlayerNo);
+                }
+                // Check current walljump situation...
+                walljumpSituation currWalljumpSituation = decideWalljumpSituation(fighterIn);
+                // ... and if no walljump is allowed...
+                if (currWalljumpSituation == wt_INVALID)
+                {
+                    // ... then disable the transition entirely.
+                    statusModule->unableTransitionTermGroup(Fighter::Status_Transition_Term_Group_Chk_Air_Wall_Jump);
+                }
+                // Otherwise, do our normal checks.
+                else
+                {
+                    // If the current action is one that's allowed to walljump natively...
+                    if (walljumpNativeTemp & playerBit)
                     {
-                        // If so, check the current state to see if we should enable the flag...
-                        soMotionModule* motionModule = moduleAccesser->m_enumerationStart->m_motionModule;
-                        if (!motionModule->isLooped() && motionModule->getFrame() >= 15.0f)
+                        // ... enable walljumping.
+                        statusModule->enableTransitionTermGroup(Fighter::Status_Transition_Term_Group_Chk_Air_Wall_Jump);
+                    }
+                    // If Walljump out of Special(Fall) is enabled...
+                    else if (mechHub::getPassiveMechanicEnabled(fighterPlayerNo, mechHub::pmid_WALLJUMP_FROM_SPECIAL))
+                    {
+                        // ... check if we're currently in a special without the walljump flag set.
+                        if (currStatus > Fighter::Status_Test_Motion && (specialWalljumpTemp & playerBit) == 0)
                         {
-                            float animProgress = mechUtil::currAnimProgress(fighterIn);
-                            float ySpeed =
-                                ftValueAccesser::getVariableFloat(moduleAccesser, ftValueAccesser::Var_Float_Kinetic_Sum_Speed_Y, 0);
-                            if (animProgress >= 0.80f
-                                || (animProgress >= 0.50f && ySpeed < -0.5f && !fighterIn->isEnableCancel()))
+                            // If so, check the current state to see if we should enable the flag...
+                            soMotionModule* motionModule = moduleAccesser->m_enumerationStart->m_motionModule;
+                            if (!motionModule->isLooped() && motionModule->getFrame() >= 15.0f)
                             {
-                                // ... and if so, turn the flag on and enable walljumping!
-                                specialWalljumpTemp |= playerBit;
-                                statusModule->enableTransitionTermGroup(Fighter::Status_Transition_Term_Group_Chk_Air_Wall_Jump);
+                                float animProgress = mechUtil::currAnimProgress(fighterIn);
+                                float ySpeed =
+                                    ftValueAccesser::getVariableFloat(moduleAccesser, ftValueAccesser::Var_Float_Kinetic_Sum_Speed_Y, 0);
+                                if (animProgress >= 0.80f
+                                    || (animProgress >= 0.50f && ySpeed < -0.5f && !fighterIn->isEnableCancel()))
+                                {
+                                    // ... and if so, turn the flag on and enable walljumping!
+                                    specialWalljumpTemp |= playerBit;
+                                    statusModule->enableTransitionTermGroup(Fighter::Status_Transition_Term_Group_Chk_Air_Wall_Jump);
+                                }
                             }
                         }
                     }
-                }
-                // Additionally, check if Button Walljumping is enabled.
-                if (mechHub::getPassiveMechanicEnabled(fighterPlayerNo, mechHub::pmid_WALLJUMP_BUTTON))
-                {
-                    // If it is, we need to impose the ledge-drop jump lockout.
-                    // First, check how many frames it's been since we dropped from ledge.
-                    u32 framesSinceLedgeGrab =
-                        ftValueAccesser::getConstantInt(moduleAccesser, ftValueAccesser::Common_Param_Int_Cliff_No_Catch_Frame, 0)
-                        - workManageModule->getInt(Fighter::Instance_Work_Int_Cliff_No_Catch_Frame);
-                    // If we're still within the defined lockout length...
-                    if (framesSinceLedgeGrab < 8)
+                    // Lastly, check if Button Walljumping is enabled...
+                    if (mechHub::getPassiveMechanicEnabled(fighterPlayerNo, mechHub::pmid_WALLJUMP_BUTTON))
                     {
-                        // ... then report that we're within the lockout still, and force-disable walljumping!
-                        OSReport_N(walljumpStatusFmt, outputTag, "Lockout Active");
-                        statusModule->unableTransitionTermGroup(Fighter::Status_Transition_Term_Group_Chk_Air_Wall_Jump);
-                    }
-                    // Otherwise, if we're out of the lockout...
-                    else if (framesSinceLedgeGrab == 8)
-                    {
-                        // ... and the current action is one that's allowed to walljump natively...
-                        if (walljumpNativeTemp & playerBit)
-                        {
-                            // ... report, and re-enable walljumping.
-                            OSReport_N(walljumpStatusFmt, outputTag, "Lockout Finished");
-                            statusModule->enableTransitionTermGroup(Fighter::Status_Transition_Term_Group_Chk_Air_Wall_Jump);
-                        }
-                    }
-
-                    // Lastly, perform the button jump attempt.
-                    tryExtendedWalljump(fighterIn);
-                }
-                // Lastly, if Walljump Once Per Airtime is on...
-                if (mechHub::getPassiveMechanicEnabled(fighterPlayerNo, mechHub::pmid_WALLJUMP_ONCE_PER_AIR))
-                {
-                    // ... then report that we're within the lockout still, and force-disable walljumping!
-                    if (walljumpSpentTemp & playerBit)
-                    {
-                        OSReport_N(walljumpStatusFmt, outputTag, "Spent");
-                        statusModule->unableTransitionTermGroup(Fighter::Status_Transition_Term_Group_Chk_Air_Wall_Jump);
+                        // ... and attempt one if so.
+                        tryButtonWalljump(fighterIn);
                     }
                 }
             }
-            // Otherwise, if we're in any other situation...
-            else
-            {
-                // ... reset the spent and special walljump flags.
-                walljumpSpentTemp &= ~playerBit;
-                specialWalljumpTemp &= ~playerBit;
-            }
 
-            perPlayerFlags[pf_WallJumpSpent] = walljumpSpentTemp;
             perPlayerFlags[pf_WalljumpOutOfSpecialEnabled] = specialWalljumpTemp;
         }
     }
-
     void onStatusChangeCallback(Fighter* fighterIn)
     {
         u32 fighterPlayerNo = fighterHooks::getFighterPlayerNo(fighterIn);
@@ -173,14 +206,10 @@ namespace rmWalljumpTweaks
 
             const u32 playerBit = 1 << fighterPlayerNo;
             u32 walljumpNativeTemp = perPlayerFlags[pf_WalljumpAllowedNative];
-            u32 walljumpSpentTemp = perPlayerFlags[pf_WallJumpSpent];
             u32 specialWalljumpTemp = perPlayerFlags[pf_WalljumpOutOfSpecialEnabled];
 
             walljumpNativeTemp &= ~playerBit;
-            if (statusModule->isEnableTransitionTermGroup(Fighter::Status_Transition_Term_Group_Chk_Air_Wall_Jump))
-            {
-                walljumpNativeTemp |= playerBit;
-            }
+            walljumpNativeTemp |= statusModule->isEnableTransitionTermGroup(Fighter::Status_Transition_Term_Group_Chk_Air_Wall_Jump) << fighterPlayerNo;
 
             u32 currStatus = statusModule->getStatusKind();
 
@@ -216,39 +245,35 @@ namespace rmWalljumpTweaks
                     }
                 }
                 // Additionally, if Walljump Once Per Airtime is on...
-                if (mechHub::getPassiveMechanicEnabled(fighterPlayerNo, mechHub::pmid_WALLJUMP_ONCE_PER_AIR))
+                if (mechHub::getPassiveMechanicEnabled(fighterPlayerNo, mechHub::pmid_WALLJUMP_SAME_DIR_RESTRICTION))
                 {
                     // ... check if we've just entered the Walljump status...
                     if (currStatus == Fighter::Status_Wall_Jump)
                     {
-                        // ... and mark that we've spent our walljump, if so.
-                        walljumpSpentTemp |= playerBit;
+                        perPlayerPrevWalljump[fighterPlayerNo] = (moduleAccesser->m_enumerationStart->m_postureModule->getLr() > 0.0f) ? wt_RIGHT : wt_LEFT;
                     }
                     // Alternatively, if we've just been hit...
                     else if (mechUtil::isDamageStatusKind(currStatus))
                     {
-                        // ... then *unset* the spent bit instead!
-                        walljumpSpentTemp &= ~playerBit;
+                        perPlayerPrevWalljump[fighterPlayerNo] = wt_NONE;
+                        OSReport_N("%s[P%d] Walljump Restored after Damage!\n", outputTag, fighterPlayerNo);
                     }
-                    // Lastly, regardless of what we're transitioning into, if the walljump spent flag is on...
-                    if (walljumpSpentTemp & playerBit)
+                    if (decideWalljumpSituation(fighterIn) <= wt_NONE)
                     {
-                        // ... disable walljumping in the new action, and signal that it's disallowed! 
-                        OSReport_N(walljumpStatusFmt, outputTag, "Spent, Status Change");
                         statusModule->unableTransitionTermGroup(Fighter::Status_Transition_Term_Group_Chk_Air_Wall_Jump);
                     }
                 }
+
             }
             // Otherwise, if we're not in the air...
             else
             {
                 // ... reset both flags.
-                walljumpSpentTemp &= ~playerBit;
                 specialWalljumpTemp &= ~playerBit;
+                perPlayerPrevWalljump[fighterPlayerNo] = wt_NONE;
             }
 
             perPlayerFlags[pf_WalljumpAllowedNative] = walljumpNativeTemp;
-            perPlayerFlags[pf_WallJumpSpent] = walljumpSpentTemp;
             perPlayerFlags[pf_WalljumpOutOfSpecialEnabled] = specialWalljumpTemp;
         }
     }
