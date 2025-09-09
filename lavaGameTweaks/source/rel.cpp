aaa6b93d6ba14e482139724a5f0ff52a1aaa5c5e
QuickLava <fastmovingvolcanomatter@gmail.com>
2025-09-08 19:13:54 -0700

    Updated Version Number

--- lavaGameTweaks/source/rel.cpp
+++ lavaGameTweaks/source/rel.cpp
@@ -9,7 +9,7 @@ namespace Syringe
     const PluginMeta META = {
         "lavaGameTweaks",
         "QuickLava",
-        Version("0.7.0"),
+        Version("0.7.1"),
         Version(SYRINGE_VERSION)};
 
     extern "C"
